6023f91b79476cc2d5747be8a9c535bc9c2af66f
Colin Surprenant <colin.surprenant@gmail.com>
2015-05-05 17:26:02 -0400

    fix LOGSTASH_HOME build problem

--- spec/filters/mutate_spec.rb
+++ spec/filters/mutate_spec.rb
@@ -3,6 +3,13 @@
 require "logstash/devutils/rspec/spec_helper"
 require "logstash/filters/mutate"
 
+# running mutate which depends on grok  outside a logstash package means
+# LOGSTASH_HOME will not be defined, so let's set it here
+# before requiring the grok filter
+unless LogStash::Environment.const_defined?(:LOGSTASH_HOME)
+  LogStash::Environment::LOGSTASH_HOME = File.expand_path("../../../", __FILE__)
+end
+
 describe LogStash::Filters::Mutate do
 
   context "config validation" do
