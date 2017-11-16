dc1851ec066d97e70c941c8bad0dd14f96558357
Abdul Haseeb Hussain <a.h.hussain@edu.salford.ac.uk>
2017-11-16 22:41:50 +0000

    Forgot to uncomment imports

--- spec/filters/mutate_spec.rb
+++ spec/filters/mutate_spec.rb
@@ -1,14 +1,14 @@
 # encoding: utf-8
 
-#require "logstash/devutils/rspec/spec_helper"
-#require "logstash/filters/mutate"
+require "logstash/devutils/rspec/spec_helper"
+require "logstash/filters/mutate"
 
 # running mutate which depends on grok  outside a logstash package means
 # LOGSTASH_HOME will not be defined, so let's set it here
 # before requiring the grok filter
-#unless LogStash::Environment.const_defined?(:LOGSTASH_HOME)
- # LogStash::Environment::LOGSTASH_HOME = File.expand_path("../../../", __FILE__)
-#end
+unless LogStash::Environment.const_defined?(:LOGSTASH_HOME)
+  LogStash::Environment::LOGSTASH_HOME = File.expand_path("../../../", __FILE__)
+end
 
 # temporary fix to have the spec pass for an urgen mass-publish requirement.
 # cut & pasted from the same tmp fix in the grok spec
